1c707776f5f426c71af96354f584e3866f6317bb
Binod Bist <thebinodbist@gmail.com>
2025-11-24 15:39:53 +0545

    footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+     <div className='footer'>
+                <div>
+                    <img src="/logo.png" alt="" className='logo-img' />
+                    <h3>Gravure Link</h3>
+                    <p>Nepal's First Modern Technology</p>
+                </div>
+                <div>
+                    © Gravure Link Nepal Pvt. Ltd. 2017
+                    <br />
+                    All Rights Reserved
+                    <br />
+                    Technology Partner |PRACAS
+                </div>
+                <div>
+                    <a href="#">Site Map</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+
+                </div>
+                <div>
+                    <a href="#">Portfolio</a>
+                    <a href="#">Order</a>
+                </div>
+            </div>
+  )
+}
+
+export default Footer
