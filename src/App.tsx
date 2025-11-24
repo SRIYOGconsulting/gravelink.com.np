033b9f38d35a3fb4f5540691c1dc21bb0ef3a446
Binod Bist <thebinodbist@gmail.com>
2025-11-24 15:54:55 +0545

    routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Logo from './pages/Logo'
+import Order from './pages/Order'
+import Portfolio from './pages/Portfolio'
+import Services from './pages/Services'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import AboutUs from './pages/AboutUs'
 
 const App = () => {
   return (
+    <>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='/logo' element={<Logo />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/prtfolio' element={<Portfolio />} />
+        <Route path='/services' element={<Services />} />
       </Routes>
+      <Footer/>
+    </>
   )
 }
 
