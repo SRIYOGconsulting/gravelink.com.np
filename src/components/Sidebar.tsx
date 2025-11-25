ead7338eaa51545234532e694bd99053972c9547
Binod Bist <thebinodbist@gmail.com>
2025-11-25 15:06:39 +0545

    sidebar updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import "./Sidebar.css"
 
-const Sidebar = () => {
+interface Props{
+  open:boolean,
+  setOpen:()=>void,
+}
+
+const Sidebar = ({open,setOpen}:Props) => {
   return (
-    <div className='container'>
+  
+    <div className={`container ${open ? 'open' : ''}`}>
       <div>
-        <a href=""><h2>Home</h2></a>
-        <a href=""><h2>About us</h2></a>
-        <a href=""><h2>Services</h2></a>
-        <a href=""><h2>Order</h2></a>
-        <a href=""><h2>Portfolio</h2></a>
-        <a href=""><h2>logo</h2></a>
-        <a href=""><h2>Contact</h2></a>
+        <a href="./home"><h2>Home</h2></a>
+        <a href="./about"><h2>About us</h2></a>
+        <a href="./services"><h2>Services</h2></a>
+        <a href="./order"><h2>Order</h2></a>
+        <a href="./portfolio"><h2>Portfolio</h2></a>
+        <a href="./logo"><h2>logo</h2></a>
+        <a href="./contact"><h2>Contact</h2></a>
       </div>
       <div className='sidebar-footer'>
          <p>
@@ -20,6 +26,7 @@ const Sidebar = () => {
          <p className='tech-partner'>Technology Partner| PRACAS</p>
       </div>
     </div>
+    
   )
 }
 
