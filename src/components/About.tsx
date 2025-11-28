29714b927d5e7a2f6b38c69ec2bcda8564110c46
Binod Bist <thebinodbist@gmail.com>
2025-11-28 17:18:27 +0545

    ...

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,29 +4,25 @@ import './About.css'
 const About = () => {
   return (
     <div className='about-container'>
-      <div>
-      <h2 className='header-text'>
-        About Us
-      </h2>
+  <div className="about-text-container">
+    <h2 className='header-text'>About Us</h2>
+    <h1>We Are The Leader In The Gravure Printing</h1>
+    <p className='about-text'>
+      Gravure Link Nepal Pvt. Ltd. A leader in the printing sector...
+    </p>
 
-      <h1>We Are The Leader In The Gravure Printing </h1>
-      <p className='about-text'>Gravure Link Nepal Pvt. Ltd. A leader in the printing sector and producing Rotogravure Electronic Printing Cylinders of the highest caliber. We serve many renowned companies around the country by offering specialized, precised, long-lasting, and artistically exceptional printing cylinders.</p>
-
-      <div className='experience'>
-        <h1 className='square'>
-           16
-        </h1>
-        <div className='working-experience'>
-          <h3>Years <br />Experience 
-          <br />Working</h3>
-        </div>
+    <div className='experience'>
+      <h1 className='square'>16</h1>
+      <div className='working-experience'>
+        <h3>Years <br />Experience <br />Working</h3>
       </div>
-      </div>
-
-       <div className='about-img'>
-          <img src="/about-home.jpg" alt="" className='about-img' />
-        </div>
     </div>
+  </div>
+
+  <div className='about-img'>
+    <img src="/about-home.jpg" alt="About Gravure Link" />
+  </div>
+</div>
   )
 }
 
