b9e84f6657f7c6d0a6010350e5334eeb4d821446
Binod Bist <thebinodbist@gmail.com>
2025-11-30 17:27:26 +0545

    portfolio fix and logo fix

--- src/pages/Logo.tsx
+++ src/pages/Logo.tsx
@@ -2,20 +2,216 @@ import React from 'react'
 import './Logo.css'
 const Logo = () => {
   return (
-    <div>
+    <div className='clinets-logo-container'>
       <h1>Client's Logo</h1>
       <div className='logo-container' >
-        <img src="/1.jpg" alt="" className='img' />
-        <img src="/2.jpg" alt="" className='img' />
-        <img src="/3.jpg" alt="" className='img' />
-        <img src="/4.jpg" alt="" className='img' />
-        <img src="/5.jpg" alt="" className='img' />
-        <img src="/6.jpg" alt="" className='img' />
-        <img src="/7.jpg" alt="" className='img' />
-        <img src="/8.jpg" alt="" className='img' />
-        <img src="/9.jpg" alt="" className='img' />
-        <img src="/10.jpg" alt="" className='img' />
-        <img src="/11.jpg" alt="" className='img' />
+        <div className='logo-img-container'>
+        <img src="/logo/1.jpg" alt="client's logo" className="clients-logo-img" />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/2.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/3.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/4.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/5.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/6.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/7.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/8.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+<div className='logo-img-container'>
+        <img src="/logo/9.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/10.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/11.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/12.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/13.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/14.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/15.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/16.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/17.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/18.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/19.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/20.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/21.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/22.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/23.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/24.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/25.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/26.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/27.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/28.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/29.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/30.jpg" alt="client's logo" className="clients-logo-img" />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/31.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/32.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/33.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/34.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/35.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/36.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/37.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/38.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/39.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/40.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/41.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/42.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/43.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/44.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/45.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/46.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/47.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/48.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/49.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/50.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/51.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/52.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/53.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/54.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/55.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/56.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/57.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/58.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/59.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/60.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/61.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/62.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/63.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/64.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/65.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="logo/66.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/67.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/68.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
+        <div className='logo-img-container'>
+        <img src="/logo/69.jpg" alt="client's logo" className="clients-logo-img"  />
+        </div>
       </div>
     </div>
   )
